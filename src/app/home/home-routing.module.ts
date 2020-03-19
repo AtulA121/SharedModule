40419005d590153dcbf9ea160f3823285be83cb5
A121 <atulpisal.ap@gmail.com>
2020-03-19 12:17:03 +0530

    added all code

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { PictureComponent } from './picture/picture.component';
 
 const routes: Routes = [
   {
     path : "",
-    loadChildren : "./home/home.module#HomeModule"
+    component : PictureComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class HomeRoutingModule { }
